fafa6d351fd3934a06c056fbc05b4e3e57cf516b
Haseeb Malik <connect2haseebmalik@gmail.com>
2023-04-24 18:34:37 +0500

    added animations in homepage.

--- lib/presentation/home/widgets/home_square_category.dart
+++ lib/presentation/home/widgets/home_square_category.dart
@@ -1,4 +1,4 @@
-import 'package:dropandgouser/shared/aniamtions/fade_animation.dart';
+import 'package:dropandgouser/shared/animations/fade_animation.dart';
 import 'package:dropandgouser/shared/constants/assets.dart';
 import 'package:dropandgouser/shared/helpers/colors.dart';
 import 'package:dropandgouser/shared/widgets/standard_text.dart';
