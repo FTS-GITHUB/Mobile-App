62cde23a368038f3dd027352782c2b6a36e6534b
Haseeb Malik <connect2haseebmalik@gmail.com>
2023-04-24 15:40:41 +0500

    search found page UI designed.

--- lib/presentation/search/search_found.dart
+++ lib/presentation/search/search_found.dart
@@ -1,10 +1,10 @@
 import 'package:dropandgouser/shared/constants/assets.dart';
-import 'package:dropandgouser/shared/extensions/extensions.dart';
 import 'package:dropandgouser/shared/helpers/colors.dart';
 import 'package:dropandgouser/shared/helpers/typography/font_weights.dart';
 import 'package:dropandgouser/shared/widgets/standard_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SearchFound extends StatelessWidget {
   const SearchFound({Key? key}) : super(key: key);
@@ -12,17 +12,26 @@ class SearchFound extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+        padding: EdgeInsets.only(
+          left: 16.w,
+          right: 16.w,
+        ),
+        primary: false,
         shrinkWrap: true,
         itemCount: 12,
         itemBuilder: (context, index) {
           return Container(
-            margin: const EdgeInsets.only(bottom: 15),
+            margin: const EdgeInsets.only(bottom: 18),
             child: Row(
               children: [
-                Image.asset(
-                  DropAndGoImages.addictions,
-                  width: 73.w,
-                  height: 70.h,
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    DropAndGoImages.addictions,
+                    width: 73.w,
+                    height: 70.h,
+                    fit: BoxFit.cover,
+                  ),
                 ),
                 10.w.horizontalSpace,
                 Column(
@@ -32,6 +41,7 @@ class SearchFound extends StatelessWidget {
                       context,
                       "ANXIETY",
                     ),
+                    6.h.verticalSpace,
                     StandardText.button(
                       "Artist Name",
                       fontSize: 10,
@@ -41,10 +51,15 @@ class SearchFound extends StatelessWidget {
                     ),
                   ],
                 ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Icon(Icons.favorite_border,),
-                )
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: SvgPicture.asset(DropAndGoIcons.favoriteOutlined),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
               ],
             ),
           );
