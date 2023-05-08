ae95260467b7b6572a0ffca4a70fcdf3bf96c07d
Haseeb Malik <connect2haseebmalik@gmail.com>
2023-05-09 00:15:52 +0500

    added search bloc for getting previous searches.

---
+++ lib/application/search/search_bloc/search_event.dart
@@ -0,0 +1,10 @@
+part of 'search_bloc.dart';
+
+abstract class SearchEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class FetchPreviousSearches extends SearchEvent {
+  FetchPreviousSearches();
+}
---
+++ lib/application/search/search_bloc/search_state.dart
@@ -0,0 +1,26 @@
+part of 'search_bloc.dart';
+
+abstract class SearchState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class SearchStateInitial extends SearchState {}
+
+class SearchStateLoading extends SearchState {}
+
+class SearchStateLoaded extends SearchState {
+  SearchStateLoaded({
+    required this.previousSearches,
+  });
+
+  final List<String> previousSearches;
+}
+
+class SearchStateError extends SearchState {
+  SearchStateError({
+    required this.message,
+  });
+
+  final String message;
+}
