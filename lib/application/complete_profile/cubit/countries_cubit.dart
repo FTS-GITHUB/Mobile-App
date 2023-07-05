a6edc21a0f89d68363d0d02e9b2adf401b156498
Zubair Ashraf <zubair@mulimobile.com>
2023-07-05 19:15:29 +0500

    fix: countries not sorted issue

--- lib/application/complete_profile/cubit/countries_cubit.dart
+++ lib/application/complete_profile/cubit/countries_cubit.dart
@@ -1,16 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CountriesCubit extends Cubit<List<String>>{
-  CountriesCubit():super([]);
+class CountriesCubit extends Cubit<List<String>> {
+  CountriesCubit() : super([]);
 
   static List<String> countries = [];
 
-  onAddCountry(String value){
+  onAddCountry(String value) {
     countries.add(value);
     emit(countries);
   }
 
-  onCountriesAdded(){
+  onCountriesAdded() {
+    countries.sort(
+      (a, b) => a.toLowerCase().compareTo(
+            b.toLowerCase(),
+          ),
+    );
     emit(countries);
   }
 }
